628a4e01a2476d695b3e85ec1fe923e89ea6d6e5
GS Gemini <me.gsbit@hsx5.com>
2024-09-24 13:16:20 -0400

    fix wildcard blog

--- nixos/common/global/mods/acme.nix
+++ nixos/common/global/mods/acme.nix
@@ -15,7 +15,7 @@
 #########################################
 
       "codefun.fyi" = {
-        extraDomainNames = [ "www.codefun.fyi" "*.codefun.fyi" "blog.codefun.fyi"  ];
+        extraDomainNames = [ "www.codefun.fyi" "*.codefun.fyi" ];
         dnsProvider = "cloudflare";
         credentialsFile = "/home/deployer/cf/cloudf";
         webroot = null;
