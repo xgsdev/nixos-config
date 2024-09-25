dc11e194bb3f696cbf1ea92516f0071304167ae7
GS Gemini <me.gsbit@hsx5.com>
2024-09-25 13:25:22 -0400

    fix

--- nixos/common/global/mods/auto-upgrade.nix
+++ nixos/common/global/mods/auto-upgrade.nix
@@ -6,8 +6,8 @@
     flags = [ " --no-write-lock-file" ];
     allowReboot = true;
     dates = "Mon *-*-* 03:01:00";
-    RandomizedDelaySec = 1800;
-    FixedRandomDelay = true;
+    randomizedDelaySec = "1800";
+    fixedRandomDelay = true;
     rebootWindow = {
       lower = "04:00";
       upper = "05:00";
